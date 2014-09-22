832bf6c6b2e3328d5a59ccf8644ffa6f1f3b0a0d
derris <derris@163.com>
2014-09-22 22:53:07 +0800

    user对象建立探索

--- db.js
+++ db.js
@@ -1,37 +1,50 @@
+var gUid = require('uuid');
+
 var sqlite3 = require('sqlite3');
 var gdb = new sqlite3.Database('exman.db');
 gdb.run("CREATE TABLE if not exists userinfo(UserName varchar(64) NOT NULL, UserPass varchar(64) NOT NULL);");
 
-var User = function(aUser){
-    username = aUser.username;
-    userpass = aUser.userpass;
+var reConnect = function(){
+    if (!gdb)  var gdb = new sqlite3.Database('exman.db');  // 时间长了可能会自动断掉。
 }
 
+var User = function(){
+    this.user = { userId: gUid.v1(),
+                    userName:"",
+                    userPass :"" };
 
-var UserOper = {    
-    save: function save(aUser, aCallback){
+    this.save = function save( aCallback){
         var err, result='';
-        try {gdb.run("INSERT INTO userinfo(username,userpass) VALUES (?, ?)",  [aUser.username, aUser.userpass]);}
+        try {gdb.run("INSERT INTO userinfo(username,userpass) VALUES (?, ?)",  [this.user.userName, this.user.userPass]);}
             catch (err) { aCallback(err,result); }            
             finally {  aCallback(null, result); }            
-    },
+    };
     //根据用户名得到用户数量
-    getUserNumByName: function getUserNumByName(aUserName, aCallback) {
+    this.getUserNumByName = function (aUserName, aCallback) {
         gdb.get("SELECT COUNT(1) AS num FROM userinfo WHERE username = ?", aUserName, function(err, row) {  if (err) { console.log("getUserNumByName Error: " + err.message); }
             aCallback(err,row);  });
-    },
+    };
     //根据用户名得到用户信息
-    getUserByUserName:function getUserNumByName(aUserName, aCallback) {
+    this.getUserByUserName = function (aUserName, aCallback) {
         gdb.get("SELECT * FROM userinfo WHERE username = ?", [aUserName], function (err, row) {
             if (err) {console.log("getUserByUserName Error: " + err.message);}
             aCallback(err,row); });        
+    };
+    this.getAllUser = function(aCallback) {
+        gdb.all("SELECT * FROM userinfo ", function (err, row) {
+            if (err) {console.log("getAllUser Error: " + err.message);}
+            aCallback(err,row); });
     }
 }
 
-var db = {
-    userOper: UserOper, 
-    user : function(aUserName, aUserPass){ return new User(aUserName, aUserPass); }
+module.exports = {
+    factory: function (aName) {
+        if (['User', 'Object'].indexOf(aName) >= 0) {
+            return eval('new ' + aName + '()')
+        }
+        else {
+            console.log("no such object " + aName);
+        }
+    }
 }
-
-module.exports = db;
 
