8f1eb7b7433ea1f893c3861f9fbdf3f6578a1377
blaczom <blaczom@gmail.com>
2014-09-23 15:58:55 +0800

    test design

--- db.js
+++ db.js
@@ -2,49 +2,74 @@ var gUid = require('uuid');
 
 var sqlite3 = require('sqlite3');
 var gdb = new sqlite3.Database('exman.db');
-gdb.run("CREATE TABLE if not exists userinfo(UserName varchar(64) NOT NULL, UserPass varchar(64) NOT NULL);");
+gdb.run("CREATE TABLE if not exists userinfo(UserId char(36), UserName varchar(64) NOT NULL, " +
+  "UserPass varchar(64) NOT NULL);");
 
 var reConnect = function(){
-    if (!gdb)  var gdb = new sqlite3.Database('exman.db');  // 时间长了可能会自动断掉。
+  if (!gdb)  var gdb = new sqlite3.Database('exman.db');  // 时间长了可能会自动断掉。
 }
 
 var User = function(){
-    this.user = { userId: gUid.v1(),
-                    userName:"",
-                    userPass :"" };
-
-    this.save = function save( aCallback){
-        var err, result='';
-        try {gdb.run("INSERT INTO userinfo(username,userpass) VALUES (?, ?)",  [this.user.userName, this.user.userPass]);}
-            catch (err) { aCallback(err,result); }            
-            finally {  aCallback(null, result); }            
-    };
-    //根据用户名得到用户数量
-    this.getUserNumByName = function (aUserName, aCallback) {
-        gdb.get("SELECT COUNT(1) AS num FROM userinfo WHERE username = ?", aUserName, function(err, row) {  if (err) { console.log("getUserNumByName Error: " + err.message); }
-            aCallback(err,row);  });
-    };
-    //根据用户名得到用户信息
-    this.getUserByUserName = function (aUserName, aCallback) {
-        gdb.get("SELECT * FROM userinfo WHERE username = ?", [aUserName], function (err, row) {
-            if (err) {console.log("getUserByUserName Error: " + err.message);}
-            aCallback(err,row); });        
-    };
-    this.getAllUser = function(aCallback) {
-        gdb.all("SELECT * FROM userinfo ", function (err, row) {
-            if (err) {console.log("getAllUser Error: " + err.message);}
-            aCallback(err,row); });
+  this.userId =  gUid.v1();
+  this.userName = "";
+  this.userPass = "";
+  User.prototype.save = function (aCallback){
+    try {
+      gdb.run("INSERT INTO userinfo(userid, username,userpass) VALUES (?,?,?)",
+        [this.userId, this.user.userName, this.user.userPass],
+        function (err, row){
+          if (err) { console.log("save Error: " + err.message);  }
+          aCallback(err, row);
+        }
+      );
     }
+    catch (err) { aCallback(err,result); }
+    finally {  aCallback(null, result); }
+  };
+  User.prototype.getNumByName = function (aUserName, aCallback) {
+    try {
+      gdb.get("SELECT COUNT(1) AS num FROM userinfo WHERE username = ?", aUserName,
+        function (err, row) {
+          if (err) { console.log("getNumByName Error: " + err.message); }
+          aCallback(err, row);
+        }
+      );
+    }
+    catch (err) { aCallback(err,result); }
+    finally {  aCallback(null, result); }
+  }
+  User.prototype.getByName = function (aUserName, aCallback) {
+    try {
+      gdb.get("SELECT * FROM userinfo WHERE username = ?", [aUserName],
+        function (err, row) {
+          if (err) {  console.log("getByName Error: " + err.message); }
+          aCallback(err, row);
+        }
+      );
+    }
+    catch (err) { aCallback(err,result); }
+    finally {  aCallback(null, result); }
+  }
+  User.prototype.getAllUser = function (aCallback) {
+    try {
+      gdb.all("SELECT * FROM userinfo ", function (err, row) {
+        if (err) {  console.log("getAllUser Error: " + err.message);  }
+        aCallback(err, row);
+      });
+    }
+    catch (err) { aCallback(err,result); }
+    finally {  aCallback(null, result); }
+  }
 }
 
 module.exports = {
-    factory: function (aName) {
-        if (['User', 'Object'].indexOf(aName) >= 0) {
-            return eval('new ' + aName + '()')
-        }
-        else {
-            console.log("no such object " + aName);
-        }
+  factory: function (aName) {
+    if (['User', 'Object'].indexOf(aName) >= 0) {
+      return eval('new ' + aName + '()')
     }
+    else {
+      console.log("no such object " + aName);
+    }
+  }
 }
 
