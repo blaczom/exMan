2d866c41a04c86dac26b2d88dfcdfc9d026acab4
blaczom <blaczom@gmail.com>
2014-09-25 16:19:38 +0800

    自动创建相关表.

--- db.js
+++ db.js
@@ -2,24 +2,23 @@ var gUid = require('uuid');
 var sqlite3 = require('sqlite3');
 
 // if exist exman.db, means the sql ddl have been execute.
-var path = require('path');
-var l_dbfile = path.dirname(process.argv[1]) + path.sep + 'exman.db';
-console.log(l_dbfile);
-if (!path.exists(l_dbfile))
+var fs = require('fs');
+
+if (!fs.exists('exman.db'))
 {
-  console.log("no databse file. will create it.")
+  console.log("no databse file. will create it.");
   var l_run = [];
   l_run.push( "CREATE TABLE if not exists USER(UUID CHAR(36), NICKNAME NVARCHAR2(36) NOT NULL, " +
     " PASS CHAR(36) NOT NULL, REMPASS BOOL, MOBILE NVARCHAR2(20), EMAIL NVARCHAR2(80), IDCARD NVARCHAR2(30), " +
     " UPMAN NVARCHAR2(36), LEVEL INTEGER, GRANT INTEGER  );");
-  l_run.push("CREATE UNIQUE INDEX [pk_usernick] ON [USER] ([NICKNAME]);");
+  l_run.push("CREATE UNIQUE INDEX if not exists  [pk_usernick] ON [USER] ([NICKNAME]);");
 
   l_run.push("CREATE TABLE if not exists TASK(UUID CHAR(36), UPTASK CHAR(36), START DATETIME NOT NULL, " +
     " FINISH DATETIME NOT NULL, STATE NVARCHAR2(8), OWNER NVARCHAR2(36) NOT NULL, LEVEL INTEGER NOT NULL, " +
     " PRIVATE BOOLEAN);");
-  l_run.push("CREATE INDEX [idx_task_owner] ON [TASK] ([OWNER] ASC);");
-  l_run.push("CREATE INDEX [idx_task_uuid] ON [TASK] ([UUID] ASC);");
-  l_run.push("CREATE INDEX [idx_task_state] ON [TASK] ([STATE] ASC);");
+  l_run.push("CREATE INDEX if not exists [idx_task_owner] ON [TASK] ([OWNER] ASC);");
+  l_run.push("CREATE INDEX if not exists [idx_task_uuid] ON [TASK] ([UUID] ASC);");
+  l_run.push("CREATE INDEX if not exists [idx_task_state] ON [TASK] ([STATE] ASC);");
 
   l_run.push("CREATE TABLE if not exists LOG(UUID CHAR(36), UPTASK CHAR(36), CREATETIME DATETIME NOT NULL,  " +
     " UPDATETIME DATETIME, STATE NVARCHAR2(8), OWNER NVARCHAR2(36) NOT NULL, PRIVATE BOOLEAN" +
@@ -31,7 +30,6 @@ if (!path.exists(l_dbfile))
   l_run.push( "CREATE TABLE if not exists TASK_MAN(TASK_ID CHAR(36), MAN_ID CHAR(36) ) ");
 //每次执行前删除一边 validate过期的东东。
   var l_init = true;
-
 }
 var gdb = new sqlite3.Database('exman.db');
 
@@ -48,22 +46,45 @@ if (l_init) {
 }
 
 var reConnect = function(){
-  if (!gdb)  var gdb = new sqlite3.Database('exman.db');  // 时间长了可能会自动断掉。
-}
+  if (!gdb)  var gdb = new sqlite3.Database('exman.db');  // 时间长了可能会自动断掉?
+};
 
 var User = function(){
-  this.userId =  gUid.v1();
-  this.userName = "";
-  this.userPass = "";
+  this.UUId =  gUid.v1();
+  this.nickName = "";
+  this.Pass = "";
+  this.rempass = true;
+  this.mobile = "";
+  this.email = "";
+  this.idcard = "" ;
+  this.upman = "";
+  this.LEVEL= 0;
+  this.grant = 0;
+
   User.prototype.save = function (aCallback){
     try {
-      gdb.run("INSERT INTO userinfo(userid, username,userpass) VALUES (?,?,?)",
-        [this.userId, this.user.userName, this.user.userPass],
-        function (err, row){
-          if (err) { console.log("save Error: " + err.message);  }
-          aCallback(err, row);
+      gdb.get("select COUNT(1) AS num from User where uuid=?", this.UUID, function(aErr, aRow){
+        if (aErr){ console.log("save Error: " + aErr.message);  }
+        if (aRaw > 0){
+          gdb.run("update User(userid, username,userpass) VALUES (?,?,?)",
+            [this.userId, this.user.userName, this.user.userPass],
+            function (err, row){
+              if (err) { console.log("save Error: " + err.message);  }
+              aCallback(err, row);
+            }
+          );
         }
-      );
+        else {
+          gdb.run("INSERT INTO User(userid, username,userpass) VALUES (?,?,?)",
+            [this.userId, this.user.userName, this.user.userPass],
+            function (err, row){
+              if (err) { console.log("save Error: " + err.message);  }
+              aCallback(err, row);
+            }
+          );
+        }
+      });
+
     }
     catch (err) { aCallback(err,result); }
     finally {  aCallback(null, result); }
@@ -79,31 +100,26 @@ var User = function(){
     }
     catch (err) { aCallback(err,result); }
     finally {  aCallback(null, result); }
-  }
-  User.prototype.getByName = function (aUserName, aCallback) {
+  };
+  User.prototype.get = function (aWhere, aCallback) {
     try {
-      gdb.get("SELECT * FROM userinfo WHERE username = ?", [aUserName],
-        function (err, row) {
-          if (err) {  console.log("getByName Error: " + err.message); }
-          aCallback(err, row);
-        }
-      );
+      if ( aWhere.length > 0 ) {
+        gdb.get("SELECT * FROM USER WHERE ", [aUserName],
+          function (err, row) {
+            if (err) {  console.log("getByName Error: " + err.message); }
+            aCallback(err, row);
+          }
+        );
+      }
     }
     catch (err) { aCallback(err,result); }
     finally {  aCallback(null, result); }
-  }
-  User.prototype.getAllUser = function (aCallback) {
-    try {
-      gdb.all("SELECT * FROM userinfo ", function (err, row) {
-        if (err) {  console.log("getAllUser Error: " + err.message);  }
-        aCallback(err, row);
-      });
-    }
-    catch (err) { aCallback(err,result); }
-    finally {  aCallback(null, result); }
-  }
-}
+  };
+};
 
+
+exports.User = User;
+/*
 module.exports = {
   factory: function (aName) {
     if (['User', 'Object'].indexOf(aName) >= 0) {
@@ -114,4 +130,4 @@ module.exports = {
     }
   }
 }
-
+*/
